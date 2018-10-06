6da3b5a229ef5f671e57164106b31bde18cc95c7
Cliff <zzcontinent@163.com>
2018-10-06 10:01:20 +0800

    add log

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utils.config import Config, get_dir_char
 import os
 import datetime
 import time
+from utils.logger import *
 
 str_and = ' && '
 
@@ -14,19 +15,20 @@ def auto_gen():
         tmpdir = Config.backup_path + get_dir_char() + v.repo_name
         if not os.path.exists(tmpdir):
             cmd = 'cd ' + Config.backup_path + str_and + 'git clone ' + v.repo_url
-            print(os.system(cmd))
-            print('git clone end ' + '*' * 20)
+            debug_log(os.system(cmd))
+            debug_log('git clone end ' + '*' * 20)
         else:
             cmd = 'cd ' + tmpdir + str_and + 'git pull'
-            print(os.system(cmd))
-            print('git pull end ' + '*' * 20)
+            debug_log(os.system(cmd))
+            debug_log('git pull end ' + '*' * 20)
     # tar backup directory
-    print('tar start' + '*' * 20)
+    debug_log('tar start' + '*' * 20)
     cmd = 'cd ' + Config.backup_path + str_and + 'cd ..' + str_and + 'tar -czvf ' + 'backup' + cur_date + '.tar.gz' + ' backup'
-    print(os.system(cmd))
+    debug_log(os.system(cmd))
 
 
 def main():
+    init_log(log_path='/var/log/GitBackup/gitbackup.log', log_level='INFO')
     while True:
         auto_gen()
         time.sleep(Config.timing_backup)

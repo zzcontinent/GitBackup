9d864dd0c6c4081b30f86413455d9aa2350107d6
Cliff <zzcontinent@163.com>
2018-10-17 14:23:12 +0800

    update

--- utils/config.py
+++ utils/config.py
@@ -22,7 +22,10 @@ class GitInfo:
         self.repo_name = repo_name
         if not repo_name:
             i = repo_url.rfind('/')
-            self.repo_name = repo_url[i + 1:].strip('.git')
+            self.repo_name = repo_url[i + 1:]
+            ii = self.repo_name.rfind('.git')
+            if -1 != ii:
+                self.repo_name = self.repo_name[:ii]
         self.user_name = user_name
         self.passwd = passwd
         if user_name and passwd:
@@ -34,15 +37,18 @@ class GitInfo:
 class Config:
     repos = [
         GitInfo(repo_url='https://github.com/zzcontinent/CliffMemSQL'),
+        GitInfo(repo_url='https://github.com/zzcontinent/CliffQueue'),
         GitInfo(repo_url='https://github.com/zzcontinent/GitBackup'),
         GitInfo(repo_url='https://github.com/zzcontinent/linaro-ubuntu-axi-driver-test'),
         GitInfo(repo_url='https://github.com/zzcontinent/ipsniffer'),
         GitInfo(repo_url='https://github.com/zzcontinent/EthernetPortSpeedTest'),
-        GitInfo(repo_url='https://github.com/zzcontinent/services'),
+        GitInfo(repo_url='https://github.com/zzcontinent/services.git'),
+        GitInfo(repo_url='https://github.com/zzcontinent/pyutils.git'),
+        GitInfo(repo_url='http://git.woda.ink/woda/services/BrokerAssistant/BA_General', user_name='cliff', passwd='xxx'),
         GitInfo(repo_url='http://git.woda.ink/dw/metadata', user_name='cliff', passwd='xxx'),
         GitInfo(repo_url='http://git.woda.ink/dw/ods_controller', user_name='cliff', passwd='xxx'),
         GitInfo(repo_url='http://git.woda.ink/dw/dw-etl', user_name='cliff', passwd='xxx'),
     ]
 
     backup_path = get_backup_path()
-    timing_backup = 60 * 60 * 1
+    timing_backup = 60 * 60 * 1 
